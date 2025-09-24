0864c0df414db4e701905d02d5b1e7586f69a301
pabitra3362 <pmohanty1435@gmail.com>
2025-09-24 23:05:26 +0530

    chore: update dependencies and add Sentry configuration
    - Added @sentry/nextjs to package.json for error tracking and performance monitoring.
    - Created sentry.edge.config.js for Sentry initialization in edge features.
    - Created sentry.server.config.js for Sentry initialization on the server.
    - Updated sonner package from version 2.0.3 to 2.0.7.

--- app/pages/shayari/page.js
+++ app/pages/shayari/page.js
@@ -1,5 +1,5 @@
 // app/shayari/page.js
-"use client"
+"use client";
 import ShayariClientList from "@/components/ShayariClientList";
 import { getAllShayari } from "@/services/shayariService";
 import { useEffect, useState } from "react";
@@ -7,22 +7,20 @@ import { useSelector } from "react-redux";
 import { toast } from "sonner";
 import Loader from "@/components/Loder";
 
-
 export default function ShayariPage() {
-
   const [shayaries, setShayaries] = useState([]);
   const [loading, setLoading] = useState(true);
-  const { user } = useSelector(state => state.user);
+  const { user } = useSelector((state) => state.user);
 
-  useEffect(()=>{
-    async function fetchShayari(){
+  useEffect(() => {
+    async function fetchShayari() {
       try {
-        const response = await getAllShayari({userId: user?.id});
+        const response = await getAllShayari({ userId: user?.id });
 
         setShayaries(response.shayaries);
       } catch (error) {
-        toast.error(error.response?.data?.message || error.message,{
-          position: "top-right"
+        toast.error(error.response?.data?.message || error.message, {
+          position: "top-right",
         });
       } finally {
         setLoading(false);
@@ -30,14 +28,22 @@ export default function ShayariPage() {
     }
 
     fetchShayari();
-  },[user])
+  }, [user]);
 
   return (
     <div className="max-w-4xl mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-[#eea679b0] mb-6 text-center">Explore Shayaris</h1>
-      {
-        loading ? <div className="min-h-screen flex justify-center items-center"><Loader /></div> : <div className="max-w-4xl mx-auto"><ShayariClientList shayaries={shayaries} /></div>
-      }
+      <h1 className="text-3xl font-bold text-[#eea679b0] mb-6 text-center">
+        Explore Shayaris
+      </h1>
+      {loading ? (
+        <div className="min-h-screen flex justify-center items-center">
+          <Loader />
+        </div>
+      ) : (
+        <div className="max-w-4xl mx-auto">
+          <ShayariClientList shayaries={shayaries} />
+        </div>
+      )}
     </div>
   );
 }
